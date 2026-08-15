f2a1b0a78bd28570ac45cff2e8a188f777bdfd14
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2026-08-15 22:17:17 +0530

    Delete anchor/target directory

--- anchor/target/sbpf-solana-solana/release/crud.d
+++
@@ -1,1 +0,0 @@
-/home/ansh/crud/anchor/target/sbpf-solana-solana/release/crud.so: /home/ansh/crud/anchor/programs/crud-app/src/lib.rs
--- anchor/target/sbpf-solana-solana/release/libcounter.d
+++
@@ -1,1 +0,0 @@
-/home/ansh/crud/anchor/target/sbpf-solana-solana/release/libcounter.rlib: /home/ansh/crud/anchor/programs/crud-app/src/lib.rs
